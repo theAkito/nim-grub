adea3b8389fb848d521110dca4778c4f7c6d96f4
Akito <the@akito.ooo>
2022-04-29 23:58:56 +0200

    Extend Command Type

--- grub/opt/kind/commandtype.nim
+++ grub/opt/kind/commandtype.nim
@@ -2,3 +2,28 @@ type
   BackgroundImageMode* = enum
     stretch,
     normal
+  GptSyncStatus* = enum
+    active,
+    inactive
+  HashType* = enum
+    adler32,
+    crc64,
+    crc32,
+    crc32rfc1510,
+    crc24rfc2440,
+    md4,
+    md5,
+    ripemd160,
+    sha1,
+    sha224,
+    sha256,
+    sha384,
+    sha512,
+    tiger,
+    tiger2,
+    tiger192,
+    whirlpool
+  PartToolCommand* = enum
+    boot,
+    `type`,
+    hidden
